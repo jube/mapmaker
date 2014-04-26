02179683179fe08a3e7a44b74c8b9351610fd068
Julien Bernard <julien.bernard@univ-fcomte.fr>
2014-04-26 20:43:32 +0200

    remove useless code

--- src/bin/akagoria-map/akagoria-map.cc
+++ src/bin/akagoria-map/akagoria-map.cc
@@ -180,32 +180,6 @@ namespace {
       return all_terrains;
     }
 
-#define TILESET_SIZE 256
-
-#if 0
-    void output_to_ppm(std::ostream& file) const {
-      tilemap map(2 * TILESET_SIZE, TILESET_SIZE);
-
-      for (tilemap::size_type x = 0; x < TILESET_SIZE; ++x) {
-        for (tilemap::size_type y = 0; y < TILESET_SIZE; ++y) {
-          double altitude = 1.0 - static_cast<double>(y) / TILESET_SIZE;
-          double humidity = static_cast<double>(x) / TILESET_SIZE;
-
-          map(x, y).set_biome(this->compute_biome(altitude, humidity, false));
-          map(x + TILESET_SIZE, y).set_biome(this->compute_biome(altitude, humidity, true));
-        }
-      }
-
-//       auto colored = biomize(biomize::kind::SIMPLE)(map, *this);
-//       colored.output_to_ppm(file);
-    }
-
-    void output_to_ppm(const std::string& filename) const {
-      std::ofstream file(filename);
-      return output_to_ppm(file);
-    }
-#endif
-
     static biomeset whittaker() {
       biomeset set;
       /*
